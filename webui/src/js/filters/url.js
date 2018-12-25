d8ace930edbadcabbc3410623de094f644454b66
wuxiaoxiong1990 <wxx1990@gmail.com>
2018-12-25 16:53:09 +0800

    delete webui

--- webui/src/js/filters/url.js
+++
@@ -1,7 +0,0 @@
-import angular from "angular";
-
-export default angular
-  .module("webui.filters.url", ["webui.services.utils"])
-  .filter("encodeURI", function() {
-    return window.encodeURI;
-  }).name;
--- webui/src/js/services/deps.js
+++
@@ -1,9 +0,0 @@
-import angular from "angular";
-import $ from "jquery";
-import _ from "lodash";
-
-export default angular
-  .module("webui.services.deps", [])
-  .value("$", $)
-  .value("$_", _)
-  .value("$json", JSON).name;
